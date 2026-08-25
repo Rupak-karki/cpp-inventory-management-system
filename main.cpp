b6762501b41eb3aa7f6bf2fe817df5c7a98eb684
Rupak-karki <karkirupak137@gmail.com>
2026-08-25 23:14:18 +0545

    feat: add product details on inventory file

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ using namespace std;
 void addProduct();
 void viewInventory();
 
+// Global variables to hold information temporarily during input/output
+string name, productID, batch, price, qty, expiry; 
+
 int main() {
     while (true) {
         int choice;
@@ -21,11 +24,11 @@ int main() {
         cout << "\n\t2. View Inventory";
         cout << "\n\t3. Exit";
         cout << "\n\n\tChoose an option: ";
-        
-        if (!(cin >> choice)) { 
-            // Fixes infinite loop bug if users type text instead of numbers
-            cin.clear(); 
+
+        if (!(cin >> choice)) {
+            cin.clear();
             cin.ignore(10000, '\n');
+
             cout << "\n\tInvalid input! Please enter a number 1-3.\n";
             cout << "\n\tPress any key to return to the menu...";
             getch();
@@ -48,7 +51,6 @@ int main() {
             default:
                 cout << "\n\tInvalid choice.";
                 cout << "\n\tPress any key to return to the menu...";
-
                 getch();
         }
     }
@@ -56,15 +58,47 @@ int main() {
 
 void addProduct() {
     system("cls");
+
+    ofstream file("inventory.txt", ios::app);
+
+    if (!file) {
+        cout << "\n\tUnable to open inventory file.";
+        cout << "\n\tPress any key to return to the menu.";
+        getch();
+        return;
+    }
     
+    cout << "\n\tEnter product details\n\n";
+    cout << "\n\tEnter product name: ";
+    cin >> name;
     
+    cout << "\n\tProduct ID: ";
+    cin >> productID;
+    
+    cout << "\tEnter batch number: ";
+    cin >> batch;
+
+    cout << "\tEnter price: ";
+    cin >> price;
+
+    cout << "\tEnter quantity: ";
+    cin >> qty;
+    
+    cout << "\tExpiry Date (yyyymmdd): ";
+	 cin >> expiry;
+
+    // Write space-separated values onto a new line
+    file << name << " " << productID << " " << batch << " " << price << " " << qty << " " << expiry << endl;
+
+    file.close();
+
+    cout << "\n\tProduct added successfully.";
     cout << "\n\tPress any key to return to the menu.";
     getch();
 }
 
 void viewInventory() {
     system("cls");
-    
 
     cout << "\n\tPress any key to return to the menu.";
     getch();
